69aa12b2da16b0b15de79ed9e00fbb9a14dab45d
Norbert Thies <norbert@taz.de>
2020-04-23 11:17:41 +0200

    Logo Launchscreen and Startup view replaced by simple Spinner on black background - arghh

--- taz.neo/Views/StartupView.swift
+++ taz.neo/Views/StartupView.swift
@@ -8,9 +8,62 @@
 import UIKit
 import NorthLib
 
-let TazRot = UIColor.rgb(0xd50d2e)
+public let TazRot = UIColor.rgb(0xd50d2e)
 
-class StartupView: UIView {
+/// A StartupView simply shows some content and allows "some" animation
+/// of this content
+public protocol StartupView where Self: UIView {
+  var isAnimating: Bool { get set }
+}
+
+/// A SpinnerView show a Spinner on a black (by default) background
+public class SpinnerStartupView: UIView, StartupView {
+  
+  private var spinner = UIActivityIndicatorView()
+  public var isAnimating: Bool {
+    get { return spinner.isAnimating }
+    set {
+      if newValue { 
+        self.isHidden = false
+        spinner.startAnimating() 
+      } 
+      else { 
+        spinner.stopAnimating() 
+        self.isHidden = true
+      }
+    }
+  }
+  
+  private func setup() {
+    if #available(iOS 13, *) { 
+      spinner.style = .large 
+      spinner.color = .white
+    }
+    else { spinner.style = .whiteLarge }
+    spinner.hidesWhenStopped = true
+    addSubview(spinner)
+    pin(spinner.centerX, to: self.centerX)
+    pin(spinner.centerY, to: self.centerY)
+    self.bringSubviewToFront(spinner)
+    self.backgroundColor = .black
+    self.isAnimating = false
+  }
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setup()
+  }
+
+} // SpinnerStartupView
+
+/// This StatupView shows the taz logo which shrinks/expands when the animation
+/// is switched on
+class LogoStartupView: UIView, StartupView {
   
   private var startupLogo: UIImage?
   private var imageView: UIImageView?
@@ -21,16 +74,13 @@ class StartupView: UIView {
   private var animationTimer: Timer?
   public var isAnimating: Bool = false {
     didSet { 
-      if isAnimating { animate() } 
-      else { animationTimer?.invalidate() }
-    }
-  }
-  public var showLogo: Bool = true {
-    didSet {
-      if showLogo { imageView?.isHidden = false }
-      else {
-        isAnimating = false
-        imageView?.isHidden = true
+      if isAnimating { 
+        self.isHidden = false
+        animate() 
+      } 
+      else { 
+        self.isHidden = true
+        animationTimer?.invalidate() 
       }
     }
   }
@@ -45,6 +95,7 @@ class StartupView: UIView {
       pin(iv.centerY, to: self.centerY)
       pinSize(factor: 0.3)
     }
+    isAnimating = false
   }
   
   func pinSize(factor: CGFloat = 1) {
