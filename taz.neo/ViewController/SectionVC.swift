e8055c6edb1437da3eb7964442eaa064cff6c69a
Norbert Thies <norbert@taz.de>
2022-05-10 13:14:18 +0200

    Check for availability of tazApi.onBookmarkChange added

--- taz.neo/ViewController/SectionVC.swift
+++ taz.neo/ViewController/SectionVC.swift
@@ -171,7 +171,11 @@ open class SectionVC: ContentVC, ArticleVCdelegate, SFSafariViewControllerDelega
     }
     Notification.receive("BookmarkChanged") { msg in
       if let art = msg.sender as? StoredArticle {
-        let js = "onBookmarkChange(\"\(art.html.name)\", \(art.hasBookmark))"
+        let js = """
+          if (typeof tazApi.onBookmarkChange === "function") {
+            tazApi.onBookmarkChange("\(art.html.name)", \(art.hasBookmark))
+          }
+        """
         self.currentWebView?.jsexec(js)
         self.debug("Called JS: \(js)")
       }
