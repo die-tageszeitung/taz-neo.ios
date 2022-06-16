3d4f6b10567c3a695e0ac8a71619aba152b48e4b
Ringo <M21>
2022-06-16 17:55:44 +0200

    Revert "re-added cherry picked showBarsOnContentChange"
    This reverts commit 366bc5ff45089c385c7a6eee973b0acbd824a48b.

--- taz.neo/ViewController/ContentVC.swift
+++ taz.neo/ViewController/ContentVC.swift
@@ -75,8 +75,6 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
   public class var topMargin: CGFloat { return 40 }
   public static let bottomMargin: CGFloat = 50
 
-  @Default("showBarsOnContentChange")
-  var showBarsOnContentChange: Bool
 
   public var feederContext: FeederContext  
   public weak var delegate: IssueInfo!
@@ -542,15 +540,6 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
       if (ratio < 0) { self?.toolBar.hide()}
       else { self?.toolBar.hide(false)}
     }
-    
-    onDisplay {[weak self]_, _  in
-      //Note: use this due onPageChange only fires on link @see WebCollectionView
-      if self?.showBarsOnContentChange == true {
-        self?.toolBar.hide(false)
-        self?.header.showAnimated()
-      }
-    }
-    
     displayUrls()
     registerForStyleUpdates()
   }
