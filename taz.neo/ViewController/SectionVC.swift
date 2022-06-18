10296373baa0e8f5d356533b2669c1575559aef3
Norbert Thies <norbert@taz.de>
2022-06-18 16:25:05 +0200

    Use async jsexec method

--- taz.neo/ViewController/SectionVC.swift
+++ taz.neo/ViewController/SectionVC.swift
@@ -175,15 +175,16 @@ open class SectionVC: ContentVC, ArticleVCdelegate, SFSafariViewControllerDelega
       if UIApplication.shared.applicationState != .active { return }
       self?.linkPressed(from: from, to: to)
     }
-    Notification.receive("BookmarkChanged") {[weak self] msg in
+    Notification.receive("BookmarkChanged") { [weak self] msg in
       if let art = msg.sender as? StoredArticle {
         let js = """
           if (typeof tazApi.onBookmarkChange === "function") {
-            tazApi.onBookmarkChange("\(art.html.name)", \(art.hasBookmark))
+            tazApi.onBookmarkChange("\(art.html.name)", \(art.hasBookmark));
           }
         """
-        self?.currentWebView?.jsexec(js)
-        self?.debug("Called JS: \(js)")
+        Task { 
+          try? await self?.currentWebView?.jsexec(js) 
+        }
       }
     }
   }
