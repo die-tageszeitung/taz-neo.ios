ce076ac2ca6331fbe7670644c2e0cfa0470cdd86
Norbert Thies <norbert@taz.de>
2022-12-04 14:07:44 +0100

    Allow bookmark updates when ArticleVC isn't visible

--- taz.neo/ViewController/ArticleVC.swift
+++ taz.neo/ViewController/ArticleVC.swift
@@ -119,7 +119,6 @@ open class ArticleVC: ContentVC {
     Notification.receive("BookmarkChanged") { [weak self] msg in
       guard let self = self else {return}
       if let cart = msg.sender as? StoredArticle,
-         self.isVisible,
          let art = self.article,
          cart.html.name == art.html.name {
          self.displayBookmark(art: art)
