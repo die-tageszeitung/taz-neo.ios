e8f6e740fdf60abb24fc24c095bc04e8902139eb
Norbert Thies <norbert@taz.de>
2023-02-27 10:21:19 +0100

    Small bugfix for xcode 14 with #selector

--- taz.neo/ViewController/ContentImageVC.swift
+++ taz.neo/ViewController/ContentImageVC.swift
@@ -181,10 +181,9 @@ public class ContentImageVC: ImageCollectionVC, CanRotate {
     super.init() 
     super.pinTopToSafeArea = false
     if #available(iOS 16.0, *) {
-      orientationClosure?.onOrientationChange(closure: {[weak self] in
-        #warning("Exchange this, when building from xcode 14")
-        self?.perform(Selector(("setNeedsUpdateOfSupportedInterfaceOrientations")))
-//        self?.setNeedsUpdateOfSupportedInterfaceOrientations()
+      orientationClosure?.onOrientationChange(closure: { [weak self] in
+        guard let self else { return }
+        self.perform(#selector(self.setNeedsUpdateOfSupportedInterfaceOrientations))
       })
     }
   }
@@ -200,9 +199,7 @@ public class ContentImageVC: ImageCollectionVC, CanRotate {
   public override func viewWillDisappear(_ animated: Bool) {
     if #available(iOS 16.0, *), let parent = self.parentViewController {
       onMainAfter {
-        parent.perform(Selector(("setNeedsUpdateOfSupportedInterfaceOrientations")))
-        #warning("Exchange this, when building from xcode 14")
-//        parent.setNeedsUpdateOfSupportedInterfaceOrientations()
+        parent.perform(#selector(parent.setNeedsUpdateOfSupportedInterfaceOrientations))
       }
     }
     super.viewWillDisappear(animated)
