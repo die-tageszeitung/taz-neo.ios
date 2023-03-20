a112b37cf213138353caa8cc4149f4d26d7699b0
Ringo <i17>
2023-03-20 10:38:21 +0100

    Fixes: #15230, crash on iPad

--- taz.neo/ViewController/SectionVC.swift
+++ taz.neo/ViewController/SectionVC.swift
@@ -224,7 +224,7 @@ open class SectionVC: ContentVC, ArticleVCdelegate, SFSafariViewControllerDelega
     
   // Define Header elements
   func setHeader(secIndex: Int) {
-    header.title = contents[secIndex].title ?? ""
+    header.title = contents.valueAt(secIndex)?.title ?? ""
     if !isStaticHeader {
       header.subTitle = issue.validityDateText(timeZone: feeder.timeZone)
       header.titletype = index == 0 ? .section0 : .section
