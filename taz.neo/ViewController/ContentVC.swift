20f30793bc621b2c5d6ce110db39326f444e6fb8
Ringo <M22>
2024-04-09 18:24:24 +0200

    rows and scroll offset seam to work on iOS15

--- taz.neo/ViewController/ContentVC.swift
+++ taz.neo/ViewController/ContentVC.swift
@@ -261,7 +261,6 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
     /// scroll visible row count right usually:
     /// contentOffset.x + sv.frame.size.width - multiColumnGap
     /// but in case of misplaced scrolling/offset, we need to 'snap' next row
-    let rowWidth = (UIWindow.size.width - multiColumnGap)/CGFloat(screenRowCount)
     let currentRow = sv.contentOffset.x/CGFloat(rowWidth)
     let wrongOffset = currentRow - floor(currentRow) > 0.1
     let offset = wrongOffset ? 1 : 0
@@ -274,6 +273,8 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
     return true
   }
   
+  var rowWidth:CGFloat { multiColumnWidth + multiColumnGap}
+  
   public override func handleLeftTap() -> Bool {
     guard isMultiColumnMode else { return super.handleLeftTap() }
     guard let sv = self.currentWebView?.scrollView  else { return false }
@@ -281,7 +282,6 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
     /// scroll visible row count right usually:
     /// contentOffset.x + sv.frame.size.width - multiColumnGap
     /// but in case of misplaced scrolling/offset, we need to 'snap' next row
-    let rowWidth = (UIWindow.size.width - multiColumnGap)/CGFloat(screenRowCount)
     let currentRow = sv.contentOffset.x/CGFloat(rowWidth)
     let wrongOffset = abs(floor(currentRow) - currentRow) > 0.1
     let offset = wrongOffset ? 1 : 0
@@ -317,8 +317,13 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
         default:
           screenRowCount = 5
       }
+      /*
+       331 = 1112/3 - 4*padding
+       331*3+4*60
+       (1112-4*padding)/3
+       */
       let screenRowCount = CGFloat(screenRowCount)
-      return ((UIWindow.size.width/screenRowCount - 2*padding), padding)
+      return (floor((UIWindow.size.width - (screenRowCount + 1)*padding)/screenRowCount + 1), padding)
     }
     /**
      Delivers wrong values?
@@ -327,10 +332,7 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
     let colMetrics = colMetrics(maxRowCount: maxRowCount)
     multiColumnGap = colMetrics.padding
     multiColumnWidth = colMetrics.width
-    print("MainWindowWidth: \(UIWindow.size.width) colWidth: \(multiColumnWidth) padding: \(multiColumnGap) rowCount:\(min(floor(maxRowCount), 5)) rowCountCalc: \(UIWindow.size.width/multiColumnWidth) maxRowCount: \(maxRowCount)")
-
-    let offset = UIWindow.isPortrait ? 47.0 : 16.0
-    let colHeight = UIWindow.size.height - UIWindow.verticalInsets - 50 - 61 - offset
+    print("#> MainWindowWidth: \(UIWindow.size.width) colWidth: \(multiColumnWidth) :: \(rowWidth) padding: \(multiColumnGap) rowCount:\(min(floor(maxRowCount), 5)) rowCountCalc: \(UIWindow.size.width/multiColumnWidth) maxRowCount: \(maxRowCount) screenRowCount: \(screenRowCount)")
     /**
      ***pretty ugly css** but:
         * content paddings&margins increase column gap
@@ -342,31 +344,24 @@ open class ContentVC: WebViewCollectionVC, IssueInfo, UIStyleChangeDelegate {
       p {
         text-align: justify;
       }
-      body, html {
-        height: \(colHeight)px;
-        overflow-y: clip;
-      }
       body {
-        padding-right: 0;
-        padding-left: 0;
-        margin-left: \(Int(2*multiColumnGap))px;
-        padding-top: 68px;
+        padding: 68px 0 50px 0;
+        height: calc(100vh - 158px);
+        margin-left: \(Int(multiColumnGap))px;
         overflow-x: scroll;
         column-width: \(Int(multiColumnWidth))px;
-        -webkit-column-width:\(Int(multiColumnWidth))px;
         width: fit-content;
         column-fill: auto;
-        column-gap: \(Int(multiColumnGap))px;
+        column-gap: 0;
         orphans: 3; /*at least 3 lines in a block at end*/
         widows: 3; /*at least 3 lines in a block at start*/
       }
       body #content.article {
-        margin-left: \(-Int(multiColumnGap))px;
-        margin-right: \(Int(multiColumnGap))px;
-        /*padding left/right must be 0 otherwise it extends the GAP*/
+        margin: 0;
+        width: \(Int(multiColumnWidth))px;
+        padding-right: \(Int(multiColumnGap))px;
         position: relative;/*important overwrite scroll.css defaults*/
         left: 0;/*important overwrite scroll.css defaults*/
-        width: initial;/*important overwrite scroll.css defaults*/
         overflow-y: hidden;
       }
       body #content.article .Autor {
