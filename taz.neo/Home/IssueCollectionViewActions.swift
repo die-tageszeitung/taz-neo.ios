ad00310a8b034b6286a33fca6f032107c7076b5a
Ringo <M22>
2025-08-01 12:36:42 +0200

    Fix Menu Handling

--- taz.neo/Home/IssueCollectionViewActions.swift
+++ taz.neo/Home/IssueCollectionViewActions.swift
@@ -67,7 +67,13 @@ extension IssueCollectionViewActions {
     actions.addMenuItem(title: "Lesestatus zurücksetzen",//Als neu/ungelesen markieren
                         icon: "sparkles",//arrow.counterclockwise
                         enabled: issue.isDownloading == false) {[weak self] _ in
-      self?.deleteIssue(issue: issue, at: indexPath)
+      LastReadBusiness.resetFor(issue: issue)
+      issue.lastPage = nil
+      issue.lastArticle = nil
+      issue.lastSection = nil
+      self?.collectionView.reloadItems(at: [indexPath])
+      self?.updateCarouselDownloadButton()
+      
     }
     
     if issue.isComplete && issue.isAudioComplete == false && issue.hasAudio
