287378738906f4e4adcf4bc7c2ac6d17372bf3e2
Ringo <M22>
2025-12-12 15:26:36 +0100

    changed default value, as discussed in the ui meeting

--- taz.neo/ConfigDefaults.swift
+++ taz.neo/ConfigDefaults.swift
@@ -62,7 +62,7 @@ private let configValues = [
   "simulateNewVersion" : "false",
   "autoPlayNext" : "true",
   "playbackRate": "1.0",
-  "edgeTapToNavigate" : "false",
+  "edgeTapToNavigate" : "true",
   /**===AUTODOWNLOAD SETTINGS FOR RESET===**/
   "autoloadOnlyInWLAN2" : "true",//renamed due previous value was false
   "autoloadPdf" : "false",
