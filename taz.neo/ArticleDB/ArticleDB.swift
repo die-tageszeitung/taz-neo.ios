cf88ff11f035a5230e3cbbffa9cf486d0d9d03f3
Ringo <M22>
2026-04-28 09:23:20 +0200

    test build disabled storedPayload.updateGlobalFiles

--- taz.neo/ArticleDB/ArticleDB.swift
+++ taz.neo/ArticleDB/ArticleDB.swift
@@ -2201,7 +2201,7 @@ public final class StoredIssue: Issue, StoredObject {
     let globalsPath = feed.feeder.globalDir.path
     var globalsSubPath = String(globalsPath.dropFirst(Database.appDir.count + 1))
     globalsSubPath = globalsSubPath.hasSuffix("/") ? String(globalsSubPath.dropLast()) : globalsSubPath
-    storedPayload.updateGlobalFiles(subdir: globalsSubPath)///fix globals subdir!
+//    storedPayload.updateGlobalFiles(subdir: globalsSubPath)///fix globals subdir!
     if let p1 = StoredPage.pageOne(issue: self) {
       let mom = StoredMoment(persistent: pr.moment!)
       mom.firstPage = p1
